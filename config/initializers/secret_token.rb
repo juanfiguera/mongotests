91deb2eb44afa9592d38429c9e858b7a093ba345
Juan Figuera <juan_figuera@hotmail.com>
2013-05-16 10:58:24 -0400

    first edit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mongotests::Application.config.secret_token = '12c72a1ac9ff8103eb317b84d7f37b9b9d006a8046c4a99e730c7ba05d2854d38cfc06bddc81c1ccf98fc60ca70a98369923320f816234dbf4f58f519c5cbf75'
+Mongotests::Application.config.secret_token = 'd4e4d14f58e1fa88e3a4793fab7da618349da81f45e6c20f284e81eb5ecad5111e5cfc419db195b3b745eec5173f340e58ce0bc243a9577b10f72b7220979900'
